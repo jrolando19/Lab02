3a4376f413cc5fb6820cc57952a45e30adb9377a
Jordy Tejada <jtejadala@unsa.edu.pe>
2023-05-28 18:32:48 -0500

    Se añade plantilla base del ejercicio 3

---
+++ RecursiveTriangle.java
@@ -0,0 +1,11 @@
+public class RecursiveTriangle {
+    public static void main(String[] args) {
+
+    }
+
+    public void trianguloRecursivo(int base) {
+        /** */
+        // Procedimiento para imprimir triángulo
+        /** */
+    }
+}
