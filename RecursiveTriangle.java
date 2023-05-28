6fa9ae472ccfff3e1f40f3c58cc8403e504c4d83
Jordy Tejada <jtejadala@unsa.edu.pe>
2023-05-28 18:52:28 -0500

    Se llena el método main para determinar el tamaño base del triángulo

--- RecursiveTriangle.java
+++ RecursiveTriangle.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class RecursiveTriangle {
     public static void main(String[] args) {
-
+        int num = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Ingrese el numero de '*' para la base: ");
+        num = scan.nextInt();
+        scan.close();
     }
 
     public void trianguloRecursivo(int base) {
