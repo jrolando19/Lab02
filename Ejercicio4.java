8c3774efe2052a580d90ea1ad0b567ddfe3cb57c
Jordy Tejada <jtejadala@unsa.edu.pe>
2023-05-28 20:15:02 -0500

    Métodos de una Lista implementados con la clase Persona

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -6,22 +6,22 @@ public class Ejercicio4 {
 
     public static void main(String[] args) {
         int opcion = 0;
-        while (opcion != 14) {
+        while (opcion != 15) {
             System.out.println("\nEscoge un número del menú");
-            System.out.println("1. Agregar elementos a la lista");
-            System.out.println("2. Agregar elemento especifico a la lista");
-            System.out.println("3. Elimina toda la lista");
-            System.out.println("4. Contiene una persona en específica");
-            System.out.println("5. Comparar una persona de la lista");
-            System.out.println("6. Que persona se encuentra en determinada posición");
-            System.out.println("7. En que posicion se encuentra determinada persona (primera aparición)");
-            System.out.println("8. Verificar si la lista está vacia");
-            System.out.println("9. En que posicion se encuentra determinada persona (última aparición)");
-            System.out.println("10. Eliminar una persona por su posición");
-            System.out.println("11. Eliminar una persona por sus datos");
-            System.out.println("12. Reemplazar una persona por otra");
-            System.out.println("13. Cuántas personas hay en la lista");
-            System.out.println("14. Que personas hay de una a otra posicion determinada");
+            System.out.println("1. Agregar elementos a la lista"); // add
+            System.out.println("2. Agregar elemento especifico a la lista"); // add
+            System.out.println("3. Elimina toda la lista"); // clear
+            System.out.println("4. Contiene una persona en específica"); // contains
+            System.out.println("5. Comparar una persona de la lista"); // equals
+            System.out.println("6. Que persona se encuentra en determinada posición"); // get
+            System.out.println("7. En que posicion se encuentra determinada persona (primera aparición)"); // indexOf
+            System.out.println("8. Verificar si la lista está vacia"); // isEmpty
+            System.out.println("9. En que posicion se encuentra determinada persona (última aparición)"); // lastIndexOf
+            System.out.println("10. Eliminar una persona por su posición"); // remove
+            System.out.println("11. Eliminar una persona por sus datos"); // remove
+            System.out.println("12. Reemplazar una persona por otra"); // set
+            System.out.println("13. Cuántas personas hay en la lista"); // size
+            System.out.println("14. Que personas hay de una a otra posicion determinada"); // subList
             System.out.println("15. Salir");
             opcion = scan.nextInt();
             switch (opcion) {
@@ -70,19 +70,20 @@ public class Ejercicio4 {
                     System.out.println(personas);
                     break;
                 case 12:
-                    System.out.println("Se eliminó la persona: " + personas.remove(registro()));
+                    System.out.println("Introduce la posicion de la persona a reemplazar");
+                    int posicion4 = scan.nextInt();
+                    System.out.println("Se reemplazó la persona: " + personas.set(posicion4, registro()));
                     System.out.println(personas);
                     break;
-                case 13:
+                case 13: // size
                     System.out.println("Hay " + personas.size() + " personas");
                     break;
-                case 14:
+                case 14: // subList
                     System.out.println("Introduce el rango de posiciones de la sublista");
                     int posicion5 = scan.nextInt();
                     int posicion6 = scan.nextInt();
                     System.out.println(personas.subList(posicion5, posicion6));
                     break;
-
             }
         }
 
