caa65fb3ae2bf798e54bd28fa8a2c110573489a3
Jordy Tejada <jtejadala@unsa.edu.pe>
2023-05-28 19:08:53 -0500

    Se implementa el método de triángulo recursivo

--- RecursiveTriangle.java
+++ RecursiveTriangle.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
 
+import javax.tools.Diagnostic;
+
 public class RecursiveTriangle {
     public static void main(String[] args) {
         int num = 0;
         Scanner scan = new Scanner(System.in);
         System.out.print("Ingrese el numero de '*' para la base: ");
         num = scan.nextInt();
+        trianguloRecursivo(num);// Se llama al método para dibujar el triángulo recusivo con una cantidad n de
+                                // "*"
         scan.close();
     }
 
-    public void trianguloRecursivo(int base) {
-        /** */
-        // Procedimiento para imprimir triángulo
-        /** */
+    public static void trianguloRecursivo(int base) {
+        if (base == 1) {
+            System.out.println("*");// Si la base es 1 se imprime un * como la cabeza del triángulo
+        } else if (base > 1) {
+            trianguloRecursivo(base - 1);
+            int i = 0;
+            while (i < base) {
+                System.out.print("*");// Imprime la cantidad de la base recursivamente - 1 hasta llegar a 1
+                i++;
+            }
+            System.out.println();
+        }
     }
 }
